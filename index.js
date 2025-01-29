5f49320c0b80c355dc35211f0927817eda766772
miguelmonterodev <miguelmonterodev@gmail.com>
2025-01-29 07:51:56 +0000

    create index.js to update new date automatically

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hello')
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const dateObj = new Date();
+const month = months[dateObj.getUTCMonth()]; //months[0-11]
+const day = dateObj.getUTCDate();
+
+let newDate;
+
+if (day == 1 || day == 21 || day == 31) {
+    newDate = `${month}, ${day}st`;
+} else if (day == 2 || day == 22) {
+    newDate = `${month}, ${day}nd`;
+} else if (day == 3 || day == 23) {
+    newDate = `${month}, ${day}rd`;
+} else {
+    newDate = `${month}, ${day}th`;
+};
+
+const spanElement = document.getElementById('date');
+spanElement.textContent = newDate;
