2315bbbd7ab31953014d298bb8c2910a8b924ec4
miguelmonterodev <miguelmonterodev@gmail.com>
2025-10-21 16:45:14 +0100

    update body of the articles

--- data.js
+++ data.js
@@ -2,12 +2,12 @@
 export const newArticles = [
     {
         title: "Learning journal",
-        body: "Today, I have been refreshing the OSI Model and a little bit of cabling and network topology",
+        body: "Today, I have been refreshing the OSI Model and a little bit of cabling and network topology.",
         date: "21-10-2025"
     },
     {
         title: "Back to IT",
-        body: "After a year learning how to code, I feel my tool box is stronger",
+        body: "After a year learning how to code, I feel my tool box is stronger.",
         date: "20-10-2025"
     },
 ]
